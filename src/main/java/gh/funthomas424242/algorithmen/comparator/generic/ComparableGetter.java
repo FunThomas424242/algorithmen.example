cd80b993fb24ce27063053fa188a51923b6e7101
FunThomas424242 <funthomas424242@gmail.com>
2016-02-21 18:18:26 +0100

    Typsicheres Comparator Framework von Heiner Kuecker aufgenommen
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

---
+++ src/main/java/gh/funthomas424242/algorithmen/comparator/generic/ComparableGetter.java
@@ -0,0 +1,13 @@
+package gh.funthomas424242.algorithmen.comparator.generic;
+
+/**
+ * Spezialisiertes Interface {@link Getter} mit Festlegung des Property-Typs auf
+ * {@link Comparable}.
+ *
+ * @param <B>
+ *            Type-Parameter, Bean-Typ
+ * @author Heiner K&uuml;cker
+ */
+interface ComparableGetter<B> extends Getter<B, Comparable<?>> {
+	// no implementation
+}
---
+++ src/test/java/gh/funthomas424242/algorithmen/comparator/test/CustomerReadAccessor.java
@@ -0,0 +1,39 @@
+package gh.funthomas424242.algorithmen.comparator.test;
+
+import java.util.Date;
+
+import gh.funthomas424242.algorithmen.comparator.generic.Getter;
+
+public class CustomerReadAccessor {
+
+	/**
+	 * Getter f�r die Property {@link #getPreName()}
+	 */
+	public static final Getter<Customer, String> PRE_NAME_GETTER = new Getter<Customer, String>() {
+		@Override
+		public String get(final Customer bean) {
+			return bean.getPreName();
+		}
+	};
+
+	/**
+	 * Getter f�r die Property {@link #getSurName()}
+	 */
+	public static final Getter<Customer, String> SUR_NAME_GETTER = new Getter<Customer, String>() {
+		@Override
+		public String get(final Customer bean) {
+			return bean.getSurName();
+		}
+	};
+
+	/**
+	 * Getter f�r die Property {@link #getBirth()}
+	 */
+	public static final Getter<Customer, Date> BIRTH_GETTER = new Getter<Customer, Date>() {
+		@Override
+		public Date get(final Customer bean) {
+			return bean.getBirth();
+		}
+	};
+
+}
