930272cb4ab74fe8f18a6fc0b2a9e4f07d1522fa
FunThomas424242 <funthomas424242@gmail.com>
2016-07-16 18:55:00 +0200

    degraph zur Strukturanalyse eingeführt
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

---
+++ src/test/java/gh/funthomas424242/algorithmen/DependencyTest.java
@@ -0,0 +1,52 @@
+package gh.funthomas424242.algorithmen;
+
+import static de.schauderhaft.degraph.check.JCheck.classpath;
+import static de.schauderhaft.degraph.check.JCheck.violationFree;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.scalatest.junit.JUnitRunner;
+
+import de.schauderhaft.degraph.configuration.NamedPattern;
+
+/**
+ * @author huluvu424242
+ *
+ * Erstellt eine Datei violations.graphml in welcher z.B. zyklische Abhängigkeiten visualisiert sind
+ * Die Graphen lassen sich visualisieren durch folgende Aktionen:
+ * 1. Öffnen im Editor yED (yworks.com/products/yed
+ * 2. Einstellung Layout/Hierarchie Layout
+ * 3. Allgemein: Links nach Rechts
+ * 4. Kantenstyle: Polyline
+ *
+ */
+//@RunWith(JUnitRunner.class)
+public class DependencyTest {
+
+    @Test
+    public void ohneZyklus() {
+        assertThat(classpath().noJars().printTo("zyklen.graphml").including(
+                "gh.funthomas424242.algorithmen.**"), violationFree());
+    }
+
+    @Test
+    @Ignore
+    public void test() {
+        assertThat(
+                classpath().noJars()
+                    .printTo("violations.graphml")
+                    .including("gh.funthomas424242.algorithmen.**")
+                    .withSlicing("part",
+                            "gh.funthomas424242.algorithmen.(*).**")
+                    .withSlicing("layer",
+                            "gh.funthomas424242.algorithmen.*.*(*).**",
+                            new NamedPattern("**", "other"))
+                    .withSlicing("module",
+                            "gh.funthomas424242.algorithmen.*.(*).**"),
+                violationFree());
+
+    }
+
+}
